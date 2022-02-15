b795e99639a2bab2732101fdbbd0f5bde742cc0f
“jelin.mi” <jelin.mi@gmail.com>
2022-02-15 22:09:10 +0100

    lab done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   /* const [foodsData, setFoodsData] = useState(foodsJSON); */
 
   // Iteration 3 | Add new food - form
-  const [isVisible, setVisible] = useState(true);
+  const [isVisible, setVisible] = useState(false);
 
   // Iteration 5 | Create add buttons
   const [todayFood, setTodayFood] = useState([]);
@@ -54,13 +54,11 @@ function App() {
       <h1>IronNutrition</h1>
       <Search onFilter={onFilter} />
       
-{/* ------- @Ale: Si dejo <button> con la condición de isVisible, se oculta el botón como quiero pero la nueva comida no se añade a la lista. Es decir, me funciona solo una cosa (mostrar / ocultar el botón "Add new food") u otra(añadir nueva comida a la lista). ------- */}
-      {!isVisible ? (
-        <Form handleVisibility={handleVisibility} addNewFood={addNewFood} />
-      ) : (
-        <button onClick={handleVisibility}>Add new food</button>
-      )}
-{/* ------- */}
+{/* Let styles to handle the 'Visibility', this way both components exist in DOM all the time. */}
+    <div className={isVisible ? 'block' : 'none'}>
+      <Form handleVisibility={handleVisibility} addNewFood={addNewFood} />
+    </div>
+    <button className={isVisible ? 'none' : 'block'} onClick={handleVisibility}>Add new food</button>
 
       {foods.map((food) => {
         return (
